1966df45d80063e6ea229c8ffedea945c22bc5c8
SHAH HUZAIFA <143290124+shahhuzaifa@users.noreply.github.com>
2023-09-25 23:01:07 +0500

    Add files via upload

---
+++ page.js
@@ -0,0 +1,138 @@
+
+//answer1 
+var userInput = "";
+var allLower = userInput.toLowerCase();
+
+//answer2 
+var a = "HELLO WORLD ";
+a = a.toLowerCase();
+console.log(a)
+
+////answer3
+var y = "booom!"
+y = y.toUpperCase();
+console.log(y)
+
+//answer4 
+var o = "SHAH HUZAIFA ";
+var r = o.toLowerCase();
+console.log(r);
+
+//answer 5
+var q = ["HELLO","WORLD"];
+var t = q[0,1].toLowerCase();
+console.log(t);
+ 
+// answer 6 
+
+var u = "yahoooo!"
+y = u.toUpperCase();
+alert(y);
+
+  // ch 22-- 25 
+
+//answer 1
+  var samewords = " captain ";
+  samewords= samewords.slice(1,2) + samewords.slice(4);
+  console.log(samewords);
+
+ // answer 2
+
+  var string = " heloo , world";
+  var ufff = string.length ;
+
+   console.log(ufff);
+
+
+//answer6 
+var text = "To be or not to be.";
+var indx = text.indexOf("be");
+console.log(indx);
+
+//answer 7 
+var tex = "To be or not to be.";
+var ind = tex.lastIndexOf("be");
+console.log(ind)
+
+//answer 8 
+ var ab = "abcde";
+ var ac = ab.charAt(2);
+ console.log(ac);
+
+
+ //answer 9
+ var hu = " this is the mosst beutiful city  ";
+ var hi = 9 ;
+ var cha = hu.charAt(hi);
+ console.log(cha);
+
+//ch 26
+var int = 3.9
+int = Math.round(int);
+console.log(int);
+
+
+var origNum = 7.8;
+var roundNum = Math.floor(origNum);
+
+console.log(roundNum);
+
+
+
+//ch27
+var randam = Math.random();
+var convert = Math.floor(randam * 50) + 1 ;
+console.log(convert);
+
+// answer
+var eo = Math.random();
+var eu = eo;
+console.log(eu);
+
+//
+
+//chap 28,29
+var toy = "4";
+var to = parseInt(toy) + 1;
+console.log(to);
+
+
+
+//answer 
+var ans = parseInt("123");
+console.log(ans);
+
+//chp30
+var nem = 0.234589;
+var nbm = nem.toFixed(4);
+console.log(nbm);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
